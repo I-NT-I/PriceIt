d7f2fd28609f995529d7bf4d4f5395fc32a72162
LeoJebran <leojebran@gmail.com>
2021-07-03 17:05:43 +0200

    feat: search adjustions

---
+++ PriceIt/PriceIt.Data/Models/SimProduct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceIt.Data.Models
+{
+    public class SimProduct
+    {
+        public int SimValue { get; set; }
+        public Product Product { get; set; }
+    }
+}
